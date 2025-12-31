65fb750062fdcd3c76550ade0224739f4a1c48b9
Jean Pierre <jalvarezidep@gmail.com>
2025-12-31 01:41:50 -0500

    register.js added before setting up prisma

--- server/src/routes/register.js
+++ server/src/routes/register.js
@@ -1,8 +1,24 @@
 const { Router } = require("express");
+const { prisma } = require("../lib/prisma")
 
 const router = Router();
 
-router.post("/register", (req, res) => {});
-
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password,
+      },
+    });
+    console.log("Usuario creado", newUser)
+    res.send("Usuario creado con éxito")
+  } catch (error) {
+    console.error("Error al crear usuario", error);
+    res.status(500).send("Error al crear usuario");
+  }
+});
 
 module.exports = router;
