7cbea2f8229c2bf8d418b8b1907fe4f6217cfc55
Jean Pierre <jalvarezidep@gmail.com>
2026-01-05 02:37:40 -0500

    modelo section done

--- client/src/components/modelo.jsx
+++ client/src/components/modelo.jsx
@@ -1,8 +1,25 @@
-import señorAudifonos from "../images/señor-audifonos.webp"
+import señorAudifonos from "../images/señor-audifonos.webp";
 
-export default function Modelo(){
-
-    return(
-        <h1>Este es modelo page</h1>
-    )
+export default function Modelo() {
+  return (
+    <section
+      className="modelo-section"
+      style={{ backgroundImage: `url(${señorAudifonos})` }}
+    >
+      <article className="modelo-container">
+        <div className="modelo-box">
+          <h1>Ajuste seguro</h1>
+          <p className="modelo-desc">
+            Con un diseño ergonómico basado en los datos de más de{" "}
+            <strong>10 mil orejas humanas</strong> para garantizar su comodidad,
+            y una optimización múltiple a nivel micrométrico, los auriculares se
+            sentirán como hechos a tu medida.
+          </p>
+          <p className="datos-modelo">
+            Datos de <span>10,000</span> orejas humanas
+          </p>
+        </div>
+      </article>
+    </section>
+  );
 }
