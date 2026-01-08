1c7a1f9109f8f9b57bbe4d8416b624e3531e4a72
Jean Pierre <jalvarezidep@gmail.com>
2026-01-08 17:35:53 -0500

    ruta de POST /product creada: para crear un producto, es protegida

--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -6,8 +6,43 @@ router.get("/product", (req, res) => {
   res.send("Get en products");
 });
 
-router.post("/product", (req, res) => {
-  res.send("Post en products");
+router.post("/product", async (req, res) => {
+  try {
+    const { name, description, price, stock } = req.body;
+    //TODO: handle imgUrl
+    const imageUrl = "not for now";
+
+    if (!name || price == null || stock == null) {
+      return res.status(400).json({ error: "Datos incompletos" });
+    }
+
+    // 1️⃣ Crear inventario
+    const inventory = await prisma.inventory.create({
+      data: {
+        stock,
+        reserved: 0,
+      },
+    });
+
+    // 2️⃣ Crear producto
+    const product = await prisma.product.create({
+      data: {
+        name,
+        description,
+        price,
+        imageUrl,
+        inventoryId: inventory.id,
+      },
+      include: {
+        inventory: true,
+      },
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al crear producto" });
+  }
 });
 
 export default router;
