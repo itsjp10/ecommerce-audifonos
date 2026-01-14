332c65fcca83778bb083098154dbf5d4e066b984
Jean Pierre <jalvarezidep@gmail.com>
2026-01-14 02:15:57 -0500

    se añade wrapper para grid layout en /cart

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -29,71 +29,73 @@ export default function Cart({ onLogout }) {
   return (
     <div className="home-page">
       <Header />
-      <main className="cart-page-content">
-        <section className="carrito-section-cart">
-          {cart.length > 0 && (
-            <header className="cart-title">Carrito de compras</header>
-          )}
-          {cart.map((item) => (
-            <article className="cart-item" key={item.id}>
-              <img className="cart-item-img" src={imgHero} alt="" />
-              <div className="cart-item-body">
-                <div className="cart-item-body-header">
-                  <h3 className="item-name">{item.product.name}</h3>
-                  <button onClick={() => removeFromCart(item.product.id)}>
-                    <Trash2 width={15} height={15} />
-                  </button>
-                </div>
-                <div className="add-to-cart">
-                  <div className="input-add">
-                    <button
-                      disabled={item.quantity === 1}
-                      onClick={() =>
-                        updateQuantity(item.product.id, item.quantity - 1)
-                      }
-                    >
-                      <Minus width={15} height={15} />
-                    </button>
-                    {item.quantity}
-                    <button
-                      onClick={() =>
-                        updateQuantity(item.product.id, item.quantity + 1)
-                      }
-                    >
-                      <Plus width={15} height={15} />
+      <main className="cart-page-wrapper">
+        <div className="cart-page-content">
+          <section className="carrito-section-cart">
+            {cart.length > 0 && (
+              <header className="cart-title">Carrito de compras</header>
+            )}
+            {cart.map((item) => (
+              <article className="cart-item" key={item.id}>
+                <img className="cart-item-img" src={imgHero} alt="" />
+                <div className="cart-item-body">
+                  <div className="cart-item-body-header">
+                    <h3 className="item-name">{item.product.name}</h3>
+                    <button onClick={() => removeFromCart(item.product.id)}>
+                      <Trash2 width={15} height={15} />
                     </button>
                   </div>
-                  <span>+10 disponibles</span>
+                  <div className="add-to-cart">
+                    <div className="input-add">
+                      <button
+                        disabled={item.quantity === 1}
+                        onClick={() =>
+                          updateQuantity(item.product.id, item.quantity - 1)
+                        }
+                      >
+                        <Minus width={15} height={15} />
+                      </button>
+                      {item.quantity}
+                      <button
+                        onClick={() =>
+                          updateQuantity(item.product.id, item.quantity + 1)
+                        }
+                      >
+                        <Plus width={15} height={15} />
+                      </button>
+                    </div>
+                    <span>+10 disponibles</span>
+                  </div>
                 </div>
-              </div>
-              <h2 className="cart-item-subtotal">
+                <h2 className="cart-item-subtotal">
+                  <span>$</span>
+                  {formatPrice(item.quantity * item.product.price)}
+                </h2>
+              </article>
+            ))}
+          </section>
+          <section className="resumen-compra">
+            <header>Resumen de compra</header>
+            <div className="resumen-compra-body">
+              <div className="productos-precio">
+                <p>Productos ({cantidadItems})</p>
                 <span>$</span>
-                {formatPrice(item.quantity * item.product.price)}
-              </h2>
-            </article>
-          ))}
-        </section>
-        <section className="resumen-compra">
-          <header>Resumen de compra</header>
-          <div className="resumen-compra-body">
-            <div className="productos-precio">
-              <p>Productos ({cantidadItems})</p>
-              <span>$</span>
-              {formatPrice(totalPrice)}
+                {formatPrice(totalPrice)}
+              </div>
+              <div className="envios-precio">
+                <p>Envios (2)</p>
+                <span>gratis</span>
+                {/*TODO: definir como serán los envios, si pagos o gratis con base a qué*/}
+              </div>
+              <div className="total-precio">
+                <h3>Total</h3>
+                <span>$</span>
+                {formatPrice(totalPrice)}
+              </div>
             </div>
-            <div className="envios-precio">
-              <p>Envios (2)</p>
-              <span>gratis</span>
-              {/*TODO: definir como serán los envios, si pagos o gratis con base a qué*/}
-            </div>
-            <div className="total-precio">
-              <h3>Total</h3>
-              <span>$</span>
-              {formatPrice(totalPrice)}
-            </div>
-          </div>
-          <button>Proceder al pago</button>
-        </section>
+            <button>Proceder al pago</button>
+          </section>
+        </div>
       </main>
       <footer>
         <button className="logout-btn" onClick={handleClick} disabled={loading}>
